818400599a69831a8b451fd8faa55e94f581b55a
hyungus06 <hyungus723@gmail.com>
2025-12-17 14:25:34 +0900

    [feat] OOP構造に基づく口座照会プログラムの実装

--- OOP_001/Program.cs
+++ OOP_001/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OOP_001
+{
+    class Account
+    {
+        private string OwnerName;
+        private string Id;
+        private int Balance;
+
+        public Account(string ownerName, string id, int balance)
+        {
+            this.OwnerName = ownerName;
+            this.Id = id;
+            this.Balance = balance;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Account Owner   : {this.OwnerName}");
+            Console.WriteLine($"Account Adress  : {this.Id}");
+            Console.WriteLine($"Account Balance : {this.Balance:N0}円");
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessAccount("高橋", "1000-0000-0000-00", 50000);
+            ProcessAccount("鈴木", "2000-0000-0000-00", 100000);
+        }
+
+        static void ProcessAccount(string OwnerName, string Id, int Balance)
+        {
+            Account user = new Account(OwnerName, Id, Balance);
+            user.ShowInfo();
+        }
+    }
+}

fd2b6ed3b0a41e63112169db5f46bc4f6c8d8029
hyungus06 <hyungus723@gmail.com>
2025-12-22 09:20:40 +0900

    [refactor] API呼出処理をServicesフォルダーに分離

---
+++ API_002/Services/ApiService.cs
@@ -0,0 +1,35 @@
+﻿using API_002.CoincheckGet;
+using System;
+using System.Text.Json;
+
+namespace API_002.Services
+{
+    class ApiService
+    {
+        public static readonly HttpClient client = new HttpClient();
+        public string baseUrl = "https://coincheck.com/api/ticker?pair=btc_jpy";
+
+        public async Task GetBtcJpyTicker()
+        {
+            try
+            {
+                string apiResponse = await client.GetStringAsync(baseUrl);
+                CoincheckJson? convertedApiResponse = JsonSerializer.Deserialize<CoincheckJson>(apiResponse);
+
+                if (convertedApiResponse != null)
+                {
+                    DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(convertedApiResponse.timestamp).AddHours(9); // UTC+9 時間設定
+
+                    Console.WriteLine("-------------------------------------------");
+                    Console.WriteLine($"げんざいBTCかかくは{convertedApiResponse.last:N0}JPYです。");
+                    Console.WriteLine($"げんざいじこくは{dateTimeOffset.ToString("yyyy-MM-dd HH:mm:ss")}です。");
+                    Console.WriteLine("-------------------------------------------");
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"エラーがはっせいしました。{error.Message}");
+            }
+        }
+    }
+}

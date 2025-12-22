ee45d15fae743dd718dc92fb4142bf4815bc352e
hyungus06 <hyungus723@gmail.com>
2025-12-22 09:17:05 +0900

    [refactor] APIデータモデルをModelsフォルダーに分離

---
+++ API_002/Models/CoincheckGet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace API_002.CoincheckGet
+{
+    public class CoincheckJson
+    {
+        public float last { get; set; }
+        public int timestamp { get; set; }
+
+        // 現在使用しない
+        // public float bid { get; set; }
+        // public float ask { get; set; }
+        // public float high { get; set; }
+        // public float low { get; set; }
+        // public float volume { get; set; }
+    }
+}
--- API_002/Program.cs
+++ API_002/Program.cs
@@ -1,22 +1,9 @@
 ﻿using System;
 using System.Text.Json;
+using API_002.CoincheckGet;
 
 namespace API_002
 {
-
-    public class CoincheckJson
-    {
-        public float last { get; set; }
-        public int timestamp { get; set; }
-
-        // 現在使用しない
-        // public float bid { get; set; }
-        // public float ask { get; set; }
-        // public float high { get; set; }
-        // public float low { get; set; }
-        // public float volume { get; set; }
-    }
-
     class ApiService
     {
         public static readonly HttpClient client = new HttpClient();
